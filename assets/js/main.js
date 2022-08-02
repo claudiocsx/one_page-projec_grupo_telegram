95c7404821d221c68c4c58eb61e29b02c3ab6452
claudiocsx <claudiosasuke@gmail.com>
2022-08-02 09:25:18 -0300

    costruindo o header nav

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const hamburgue = document.querySelector('.header__toggle')
+console.log('claudoi')
+/*______________Header_______________*/
+
+
+hamburgue.addEventListener('click', ()=>{
+ document.body.classList.toggle('open')
+})
